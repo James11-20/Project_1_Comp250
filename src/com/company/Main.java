bcc5b619a44fa3fc19ab032fc15f758a128b9549
James11-20 <J5carroll@student.bridgew.edu>
2022-09-21 02:15:31 -0400

    Done?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -10,42 +11,64 @@ public class Main {
     public static void main(String[] args) {
         Scanner file;
         Scanner keyBoard = new Scanner(System.in);
-        System.out.print("Enter File name: ");
+        System.out.print("Enter File name to load data: ");
         String inPut = keyBoard.nextLine();
         ArrayList<Person> people = new ArrayList<>();
-        Patient g = new Patient("Benjamin Thomas", 13, "Dr.LesliePark");
         try {
             file = new Scanner(new File("C:\\Users\\jcarr\\IdeaProjects\\Project_1_Comp250\\src\\com\\company\\patients.txt"));
-        }
-        catch (Exception e){
+            }
+        catch (FileNotFoundException e){
             System.out.println("File Not Found!");
             return;
         }
-        while (file.hasNextLine()) {
-            String fileData = file.toString();
-            String[] Person = fileData.split(",");
-            String name = Person[0];
-            System.out.println(name);
-          //  Patients.add(age[1]);
-          //  Patients.add(doctor[2]);
-            System.out.print("Placeholder");
-        }/*
-        while (true){
-            switch (inPut){
-                case 1: people.toString();  //printall
-                break;
-                case 2: if(people.getAge()<40){ //print if peoples age is under 40
-                    System.out.println(people);
-                    break;
-                } 
-         //       case 3: if(people.getAge()>40){   // print if people age above 40
-                    System.out.println(people);
+            while (file.hasNextLine()) {
+                try {
+                    String fileData = file.nextLine();
+                    String[] Person = fileData.split(",");
+                    String name = Person[0];
+                    String age = (Person[1]);
+                    String doctor = (Person[2]);
+                    int iAge = Integer.parseInt(age);
+                    people.add(new Patient(name,iAge,doctor));
+                }
+            catch (NumberFormatException i) {
                     break;
                 }
-           //     case 4: System.exit(0);
-            }*/
+            }
+        while (true) {
+            Scanner keyBoard2 = new Scanner(System.in);
+            System.out.println("1| Display all Patients.");
+            System.out.println("2| Display all under 40.");
+            System.out.println("3| Display all above 40.");
+            System.out.println("4| Quit");
+            System.out.print("Enter Number to display Patients or Quit!: ");
+            int inPut2 = keyBoard2.nextInt();
 
+            switch (inPut2) {
+                case 1 -> {         //Print all Patients
+                    for (Person list : people) {
+                        System.out.println(list);
+                    }
+                    System.out.println("");
+                }
+                case 2 -> {
+                    for (Person list : people) {   //print if peoples age is under 40
+                        if (list.age <= 40) {
+                            System.out.println(list);
+                        }
+                    }
+                    System.out.println("");
+                }
+                case 3 -> {
+                        for (Person list : people) {   // print if people age above 40
+                            if (list.age > 40) {
+                                System.out.println(list);
+                            }
+                        }
+                        System.out.println("");
+                    }
+                case 4 -> System.exit(0);
+            }
         }
-  //  }
+    }
 }
-
